4f2c347a70151e070e4c03904332f9ebdf81cd3d
刘鑫鹏 <1503199604@qq.com>
2020-10-23 17:53:56 +0800

    gx

--- djs/src/components/HelloWorld.vue
+++ djs/src/components/HelloWorld.vue
@@ -1,113 +1,52 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+    <el-row>
+        <el-col :span="24">
+             <el-card shadow="always">
+                 彩券
+                 <i class="el-icon-video-pause" @click="jy" width="50px"></i>
+                 <audio v-if="muted"
+                         src="http://m801.music.126.net/20201023171257/3333518f2f6f0fd3c2b6400a5e665e9d/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/4387351108/698b/39d2/91d9/dabe5d3eb8e4efaeeb43dcfc72c70f52.mp3" autoplay="autoplay"></audio>
+            </el-card>
+            <el-card class="box-card" >
+                <el-carousel indicator-position="outside" height="200px">
+                    <el-carousel-item>
+                        <img src="../img/2.jpg" width="100%" height="100%">
+                    </el-carousel-item>
+                    <el-carousel-item>
+                        <img src="../img/1.jpg" width="100%" height="100%">
+                    </el-carousel-item>
+                </el-carousel>
+            </el-card>
+        </el-col>
+    </el-row>
+
+
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    export default {
+        name: 'HelloWorld',
+        data() {
+            return {
+                show: true,
+                muted: false
+            }
+        },
+        created() {
+            // this.getData();
+        },
+        methods: {
+            getData() {
+                this.show = !this.show
+            },
+            jy(){
+                console.log(this.muted)
+                this.muted = !this.muted
+            }
+        }
     }
-  }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
